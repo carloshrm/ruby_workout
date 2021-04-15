9af29d178df7599932b13869444a3094545b0b34
chm <carloshrmoraes@gmail.com>
2021-04-15 09:48:36 -0300

    stock picker works as it should.

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,14 +1,22 @@
-
+prices = [17,3,6,9,15,8,6,1,10]
 def stock_picker(prices)       
-    high = prices[0]  
-    low = prices[0]
-    prices.each do |v|
-        high = v if v > high
-        low = v if v < low
+    high = [prices.last, 0]
+    low = [prices[0], 0]
+    prices.each_with_index do |val,ind|         
+        
+        if val > high[0] && high[1] < low[1]
+            high[0] = val
+            high[1] = ind            
+        end
+        
+        if val < low[0] && !(high[1] > low[1])            
+            low[0] = val
+            low[1] = ind
+        end
+        
     end
-    
-    return [prices.find_index(low),prices.find_index(high)]
+    return [low, high]
 end
 
-result = stock_picker([2,17,3,6,0,9,15,8,6,1])
-puts "Buy on day #{result[0]+1}, sell on day #{result[1]+1}"
+result = stock_picker([17,3,6,9,15,8,6,1,10])
+puts "Buy on day #{result[0][1]} and sell on day #{result[1][1]} for a profit of #{result[1][0] - result[0][0]}"
