aee83670f6d1f1287bec2330a427717327843aca
chm <carloshrmoraes@gmail.com>
2021-04-15 08:01:43 -0300

    improved script, no need to clean punct.

--- substrings.rb
+++ substrings.rb
@@ -2,13 +2,13 @@
 
 def substrings(input, dict)
     out_hash = Hash.new(0)    
-    clean_string = input.gsub(/[[:punct:]]/, "").downcase.split(" ")
+    clean_string = input.downcase.split(" ")
 
-    clean_string.each do |word|        
-        dict.each do |sep_input|              
-            if word.include?(sep_input)   
-                match = word.slice(sep_input)
-                out_hash[match.to_sym] += 1           
+    dict.each do |word|
+        clean_string.each do |sep_input|
+            match = sep_input.slice(word)
+            if (match != nil)
+                out_hash[match] += 1
             end
         end
     end
@@ -19,5 +19,6 @@ end
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+
 
 
