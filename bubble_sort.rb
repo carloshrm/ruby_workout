02f723bb0b9f1ebac6159ccae994da9a2ad7dadf
chm <carloshrmoraes@gmail.com>
2021-04-23 08:17:14 -0300

    formatting

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,19 @@
-def bubble_sort(numbers, notSorted = true)   
-    while notSorted        
-        notSorted = false        
-        numbers.each_with_index do |v, i|
-            next if i == 0
-            if numbers[i-1] > numbers[i]
-                numbers[i-1],numbers[i] = numbers[i],numbers[i-1]
-                notSorted = true
-            end            
-        end
-    end    
-    return numbers    
+# frozen_string_literal: true
+
+def bubble_sort(numbers, not_sorted: true)
+  while not_sorted
+    not_sorted = false
+    numbers.each_with_index do |_v, i|
+      next if i.zero?
+
+      if numbers[i - 1] > numbers[i]
+        numbers[i - 1], numbers[i] = numbers[i], numbers[i - 1]
+        not_sorted = true
+      end
+    end
+  end
+  numbers
 end
 
-numbers = [4,3,78,2,0,2,-1,32,5,6,-2]
+numbers = [4, 3, 78, 2, 0, 2, -1, 32, 5, 6, -2]
 puts bubble_sort(numbers)
